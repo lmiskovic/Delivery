7ab1a2da7941717e64e831f88dc20eb40a986537
Luka <luka22miskovic@gmail.com>
2018-01-21 23:44:13 +0100

    update

---
+++ app/src/main/java/com/example/luka/delivery/entities/onDeliveryListener.java
@@ -0,0 +1,12 @@
+package com.example.luka.delivery.entities;
+
+import java.util.List;
+
+/**
+ * Created by Luka on 21.1.2018..
+ */
+
+public interface onDeliveryListener {
+
+    void onDelivery(List<Delivery> deliveryList);
+}
